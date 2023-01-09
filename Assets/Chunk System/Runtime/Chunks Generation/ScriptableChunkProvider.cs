ebd49f6d85e2ba8b56161669803f25fb7275cab0
Michal <malpa.w.klapkach@gmail.com>
2023-01-09 20:48:47 +0100

    Remove unneeded code

--- Assets/Chunk System/Runtime/Chunks Generation/ScriptableChunkProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Bipolar.ChunkSystem.Generation
-{
-    public abstract class ScriptableChunkProvider : ScriptableObject , IChunkProvider
-    {
-        public abstract Chunk GetChunk();
-    }
-}
